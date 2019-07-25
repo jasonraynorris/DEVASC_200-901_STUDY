53d6d5e5bca1179d9578a0090a525ee16f6bce09
jasonraynorris <jason.ray.norris@gmail.com>
2019-07-25 11:59:36 -0500

    First remote tool commit

--- 1_0-1_8/data_structures.py
+++ 1_0-1_8/data_structures.py
@@ -1,141 +1,108 @@
 import json
 import sys
 import yaml
-import dicttoxml
+# import dicttoxml
 import xml.dom.minidom
 from os import system, name
 import cProfile
+from lxml import etree
 import xml.etree.ElementTree as ET
 
 def clear():
-    # for windows
     if name == 'nt':
         _ = system ('cls')
 
-        # for mac and linux(here, os.name is 'posix')
     else:
         _ = system ('clear')
 
 def end_section():
-    input ("\nPress Any Key To Continue\n")
+    input ("Press Any Key To Continue")
     clear ()
 
-testjson = {
-    "switches": [{
-                    "hostname": "switch_000001",
-                    "serial_numbers": {
-                        "chassis": "SNXA0018AX00BA"
-                    },
-                    "interfaces": [
-                                    {
-                                        "name":"ethernet0/0",
-                                        "description":"SW_000002_0/0",
-                                        "speed":1000,
-                                        "duplex":"Full",
-                                        "error_count":{
-                                            "crc":0,
-                                            "frame": 0,
-                                            "overrun": 0,
-                                        }
-                                     },
-                                    {
-                                        "name": "ethernet0/1",
-                                        "description": "SW_000002_0/1",
-                                        "speed": 10,
-                                        "duplex": "Half",
-                                        "error_count": {
-                                            "crc": 0,
-                                            "frame": 0,
-                                            "overrun": 0,
-                                        }
-                                    },
-                                ]
-                },
-                {
-                    "hostname": "switch_000002",
-                    "serial_numbers": {
-                        "chassis": "SNXA0018AX00BC"
-                    },
-                    "interfaces": [
-                                    {
-                                        "name": "ethernet0/0",
-                                        "description": "SW_000001_0/0",
-                                        "speed": 1000,
-                                        "duplex": "Full",
-                                        "error_count": {
-                                            "crc": 0,
-                                            "frame": 0,
-                                            "overrun": 0,
-                                        }
-                                    },
-                                    {
-                                        "name": "ethernet0/1",
-                                        "description": "SW_000001_0/1",
-                                        "speed": 10,
-                                        "duplex": "Half",
-                                        "error_count": {
-                                            "crc": 0,
-                                            "frame": 0,
-                                            "overrun": 0,
-                                        }
-                                    },
-                                ],
-                }]
-}
+'''Open data structures and read to string vars'''
+example_xml_file = open("example_data_set.xml","r")
+example_xml = example_xml_file.read()
+example_json_file = open("example_data_set.json","r")
+example_json = example_json_file.read()
+example_yaml_file = open("example_data_set.yaml","r")
+example_yaml = example_yaml_file.read()
 
-'''convert json to yaml'''
-testyaml = yaml.safe_dump(testjson)
+print("Let's compare the same data using different data structures")
 
-'''convert json to xml'''
-testxml = dicttoxml.dicttoxml(testjson)
 
-'''parse xml'''
-xml = xml.dom.minidom.parseString(testxml).toprettyxml()
+'''collect xml size data'''
+xml_size_results = {}
 
-print("  Let's compare data structures between XML, JSON and YAML\n")
-end_section()
+xml_size_results["xml_whitespace_string"] = example_xml.__sizeof__()
+parser = etree.XMLParser(remove_blank_text=True)
+elem = etree.XML(example_xml, parser=parser)
+#xml_size_results["xml_nowhitespace_string"] = etree.tostring(elem).__sizeof__()
+xml_size_results["xml_nowhitespace_bytes"] = sys.getsizeof(etree.tostring(elem))
 
-print(""" Here is a sample byte comparison using the following data set.\n  This notes the ammount of bytes required to transfer the same data using different structures.\n""")
+'''collect json size data'''
+json_size_results = {}
 
-print("Size of XML in bytes:%s" % sys.getsizeof(testxml))
-print("Size of JSON in bytes:%s" % (sys.getsizeof(json.dumps(testjson))))
-print("Size of YAML in bytes:%s" % sys.getsizeof(testyaml))
+json_size_results["json_whitespace_string"] = example_json.__sizeof__()
+json_no_whitespace = (json.dumps(json.loads(example_json), separators=(',', ':')))
+#json_size_results["json_nowhitespace_string"] = json_no_whitespace.__sizeof__()
+json_size_results["json_nowhitespace_bytes"] = sys.getsizeof(json_no_whitespace)
+
+'''collect yaml size data'''
+
+yaml_size_results = {}
+yaml_size_results["yaml_whitespace_string"] = example_yaml.__sizeof__()
+yaml_size_results["yaml_whitespace_bytes"] = sys.getsizeof(example_yaml)
+
+print('{:>6}{:>10}{:>10}'.format('type','pre_xfer','xfer_size'))
+print('{:>6}{:>10}{:>10}'.format('XML',xml_size_results["xml_whitespace_string"],xml_size_results["xml_nowhitespace_bytes"]))
+print('{:>6}{:>10}{:>10}'.format('JSON',json_size_results["json_whitespace_string"],json_size_results["json_nowhitespace_bytes"]))
+print('{:>6}{:>10}{:>10}'.format('YAML',yaml_size_results["yaml_whitespace_string"],yaml_size_results["yaml_whitespace_bytes"]))
 end_section()
 
 print(""" Take a look at the human readability of the same data in different structures.\n  You can copy the following data structures into a common parser.\n  Try Notepad++ and set the relevant parsing language\n""")
-if input("Print JSON:(Y/N)") == "Y":
-    print(json.dumps(testjson,indent=4))
+
 if input("Print XML(Y/N):") == "Y":
-    print(xml)
+    print(example_xml)
+if input("Print JSON:(Y/N)") == "Y":
+    print(json.dumps(example_json,indent=4))
 if input("Print YAML(Y/N):") == "Y":
-    print(testyaml)
+    print(example_yaml)
 end_section()
 
-print("  Let's look at parsing times using Python.\n  We will parse the data as strings.  We will run each parser 1000 iterations. \n")
+# print("  Let's look at parsing times using Python.\n  We will parse the data as strings.  We will run each parser 1000 iterations. \n")
+#
+# json_str = json.dumps(testjson,indent=4)
+#
+# def ProfileParseJSON():
+#     for i in range(1000):
+#         json.loads(json_str)
+#
+# def ProfileParseXML():
+#     for i in range(1000):
+#         ET.fromstring (testxml)
+#
+# def ProfileParseYAML():
+#     for i in range(1000):
+#         yaml.load(testyaml)
+#
+# if input("Parse XML:(Y/N)") == "Y":
+#     cProfile.run ('ProfileParseXML()')
+#
+# if input("Parse JSON:(Y/N)") == "Y":
+#     cProfile.run ('ProfileParseJSON()')
+#
+# if input("Parse YAML:(Y/N)") == "Y":
+#     cProfile.run ('ProfileParseYAML()')
+#
+# print("  As of the date of writing this.  You will likely see that YAML is significantly slower to parse.\n  However, it's easily readable for our human eyes.\n  In most cases, what you use is up to you as the developer.\n  I would recommend collecting some test data for yourself to make a data driven decision.")
+# print("  This concludes the current lesson.\n")
+# end_section()
 
-json_str = json.dumps(testjson,indent=4)
-
-def ProfileParseJSON():
-    for i in range(1000):
-        json.loads(json_str)
-
-def ProfileParseXML():
-    for i in range(1000):
-        ET.fromstring (testxml)
-
-def ProfileParseYAML():
-    for i in range(1000):
-        yaml.load(testyaml)
-
-if input("Parse XML:(Y/N)") == "Y":
-    cProfile.run ('ProfileParseXML()')
-
-if input("Parse JSON:(Y/N)") == "Y":
-    cProfile.run ('ProfileParseJSON()')
-
-if input("Parse YAML:(Y/N)") == "Y":
-    cProfile.run ('ProfileParseYAML()')
-
-print("  As of the date of writing this.  You will likely see that YAML is significantly slower to parse.\n  However, it's easily readable for our human eyes.\n  In most cases, what you use is up to you as the developer.\n  I would recommend collecting some test data for yourself to make a data driven decision.")
-print("  This concludes the current lesson.\n")
-end_section()
+# '''convert json to yaml'''
+# testyaml = yaml.safe_dump(testjson)
+#
+# '''convert json to xml'''
+# testxml = dicttoxml.dicttoxml(testjson)
+#
+# '''parse xml'''
+# xml = xml.dom.minidom.parseString(testxml).toprettyxml()
