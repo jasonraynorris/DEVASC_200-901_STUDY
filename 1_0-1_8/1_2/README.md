6d2cde9642b6b64eed9efd1a05556525efdf823e
deafnill <deafnill@gmail.com>
2019-07-26 11:56:48 -0500

    building structure

--- 1_0-1_8/1_2/README.md
+++
@@ -1,4 +0,0 @@
-# DEVASC_200-901_STUDY, Developed using Python3.7
-1.2 Describe parsing of common data format (XML, JSON, and YAML) to Python data structures
-
-
---
+++ main.py
@@ -0,0 +1,3 @@
+from OneZero_One_Eight.one_one.data_structures import DataStructures
+if input("1.1 Compare data formats (XML, JSON, and YAML)(Y/N)?") == "Y":
+    DataStructures()
